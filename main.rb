783293c83a80c4d462bfb2cdf46537674ae2cd3f
David Wettstein <dwettstein@gmail.com>
2016-04-07 01:27:02 +0200

    Implemented a fancy index page :P.

--- main.rb
+++ main.rb
@@ -1,16 +1,23 @@
 load 'config/sinatra.rb'
 
 before do
-  @pages = %w"ip locations connections stationboard weather stations weathers future_weathers"
+  @pages = %w"home ip locations connections stationboard weather stations weathers future_weathers"
   @author = 'Reto Schiegg, David Wettstein'
   @year = 2016
   # Define the URL of the Heroku App. Will be used within the templates.
   @heroku_api = "https://leave-the-cloud-saas-app.herokuapp.com" # No backslash at end.
+  #@heroku_api = "http://localhost:4000"
 end
 
 # Added home route.
-get '/' do
-  @page = 'index'
+get /\A\/(home)?\z/ do
+  @page = 'home'
+  @destinations = get_location_overview()
+  if @destinations != nil && (not @destinations.empty?)
+    @departing_station_name = (@destinations.first)["departing_station_name"]
+  else
+    @departing_station_name = "Not found (IP: " + get_browser_ip() + ")"
+  end
   slim :index
 end
 
@@ -59,4 +66,15 @@ def get_browser_ip()
     @client_ip = request.ip # IP also in @env['REMOTE_ADDR']
   end
   return @client_ip
+end
+
+def get_location_overview()
+  current_ip = get_browser_ip()
+  
+  weathers_url = @heroku_api + "/weathers?ip=" + current_ip
+  weathers_uri = URI(URI::encode(weathers_url))
+  weathers_response = Net::HTTP.get_response(weathers_uri)
+  weathers = JSON.parse(weathers_response.body)
+  destinations = weathers["weathers"]
+  return destinations
 end
